005c04911576674ed617e647edbff74d3dade3aa
Swen Kooij <swen@sectorlabs.ro>
2023-01-07 08:09:39 +0200

    Set up prettier

---
+++ README.md
@@ -0,0 +1,3 @@
+# roughlyfront
+
+roughlyfront is a small and mostly correct emulator for AWS Lambda@Edge. It allows you to run, test and develop your AWS Lambda@Edge functions locally.
--- src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.helloWorld = void 0;
-const helloWorld = () => {
-};
-exports.helloWorld = helloWorld;
