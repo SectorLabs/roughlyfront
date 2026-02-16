27ca4494fc8e6b4c2ae7f0dbb647f9b339f0a277
Swen Kooij <swen@sectorlabs.ro>
2026-02-16 15:41:42 +0100

    Allow `Cloudfront-Viewer-*` headers to be overriden with mock request headers

--- src/cloudfront/viewer.ts
+++ src/cloudfront/viewer.ts
@@ -27,21 +27,28 @@ export const constructViewer = (
 ): CloudFrontViewer => {
     const headers = parseIncomingMessageHeaders(incomingMessage);
 
+    const ip =
+        headers.get("x-mock-ip") ||
+        incomingMessage.socket.remoteAddress ||
+        "127.0.0.1";
+
     return {
-        ip: incomingMessage.socket.remoteAddress || "127.0.0.1",
+        ip,
         forwardedIPs: (headers.get("x-forwarded-for") || "")
             .split(",")
             .filter((ip: string) => !!ip.trim()),
-        asn: 8708,
-        country: "RO",
-        city: "Cluj-Napoca",
-        countryName: "Romania",
-        countryRegion: "CJ",
-        countryRegionName: "Cuj",
-        latitude: 46.7834818,
-        longitude: 23.5464732,
-        postalCode: "4000",
-        timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+        asn: Number(headers.get("x-mock-asn") || 8708),
+        country: headers.get("x-mock-country") || "RO",
+        city: headers.get("x-mock-city") || "Cluj-Napoca",
+        countryName: headers.get("x-mock-country-name") || "Romania",
+        countryRegion: headers.get("x-mock-country-region") || "CJ",
+        countryRegionName: headers.get("x-mock-country-region-name") || "Cuj",
+        latitude: Number(headers.get("x-mock-latitude") || 46.7834818),
+        longitude: Number(headers.get("x-mock-longitude") || 23.5464732),
+        postalCode: headers.get("x-mock-postal-code") || "4000",
+        timeZone:
+            headers.get("x-mock-time-zone") ||
+            Intl.DateTimeFormat().resolvedOptions().timeZone,
         httpVersion: incomingMessage.httpVersion,
         httpHost:
             headers.get("x-forwarded-host") ||
@@ -55,7 +62,11 @@ export const constructViewer = (
             incomingMessage.connection.encrypted
                 ? "https"
                 : "http",
-        ja3Fingerprint: "09db96991d70d43cc265f330a449fc1d",
-        ja4Fingerprint: "t13d1514h2_8daaf6152771_9a55b862dad6",
+        ja3Fingerprint:
+            headers.get("x-mock-ja3-fingerprint") ||
+            "09db96991d70d43cc265f330a449fc1d",
+        ja4Fingerprint:
+            headers.get("x-mock-ja4-fingerprint") ||
+            "t13d1514h2_8daaf6152771_9a55b862dad6",
     };
 };
